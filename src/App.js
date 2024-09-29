db61064d88463dc263f87b625a4c2107f053912c
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-09-29 19:18:39 +0200

    all-setted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Sidebar from "./components/SIdebar/Sidebar";
 import Home from "./components/Home/Home";
